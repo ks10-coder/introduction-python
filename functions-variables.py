f799935b63a645f1711a1934290c81ebdabb11f1
ks10-coder <kettisha.sandoval.10@gmail.com>
2026-09-10 13:45:42 -0600

    Eliminar espacios d/i, Primeras letras en mayúscula, Dividir los valores de las variables

--- functions-variables.py
+++ functions-variables.py
@@ -1,5 +1,8 @@
 #Ask user for their name
-name = input("What's your name? ")
+name = input("What's your name? ").strip().title()
+
+#Split user's name into first and last name
+first, last = name.split()
+
 #Say hello to the user
-print("hello,")
-print(name)
+print(f"hello, {first}")
