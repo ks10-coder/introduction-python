c4a200056fa697cd9248622fa181014f87234ed9
ks10-coder <kettisha.sandoval.10@gmail.com>
2026-09-14 13:10:39 -0600

    Calling the hello function using only a default value: "world"

--- functions-variables.py
+++ functions-variables.py
@@ -1,7 +1,7 @@
-def hello(to):
+def hello(to="world"):
     print("hello,", to)
 
 
+hello()  # Calling the hello function without any arguments, so it will use the default value "world"
 name = input("What's your name? ")
-# Calling the hello function and passing the name as an argument
 hello(name)
